8d15d873530a3fa6a7739a99c96a1e4d43ffc4eb
Jordanras96 <riantsoa96@gmail.com>
2024-03-28 18:32:25 +0300

    cleaning

--- src/main.jsx
+++ src/main.jsx
@@ -1,51 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "./index.css";
-import ErrorPage from "./error-page";
-import Login from "./pages/login/login";
-import Register from "./pages/register/register";
-import Dashboard from "./pages/app/dashboard";
-import Settings from "./pages/app/settings";
-import User from "./pages/app/user";
-import App from "./App";
-import Root from "./routes/root";
 
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <Root />,
-//     errorElement: <ErrorPage />,
-//     children: [
-//       {
-//         path: "/login",
-//         element: <Login />,
-//       },
-//       {
-//         path: "/register",
-//         element: <Register />,
-//       },
-//       {
-//         path: "/app",
-//         element: <App />,
-//         children: [
-//           {
-//             path: "/dashboard",
-//             element: <Dashboard />,
-//           },
-//           {
-//             path: "/settings",
-//             element: <Settings />,
-//           },
-//           {
-//             path: "/user",
-//             element: <User />,
-//           },
-//         ],
-//       },
-//     ],
-//   },
-// ]);
+import "./index.css";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
